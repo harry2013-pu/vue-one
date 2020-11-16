28ad07c1c977dcb385971d13866a9d6ea42ba517
harry2013-pu <harry2013me@gmail.com>
2020-11-16 16:08:49 +0800

    完成了登陆功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,19 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0
   }
+/*   "eslint.autoFixOnSave": true,  //  启用保存时自动修复,默认只支持.js文件
+  "eslint.validate": [
+      "javascript",  //  用eslint的规则检测js文件
+      {
+          "language": "vue",   // 检测vue文件
+          "autoFix": true   //  为vue文件开启保存自动修复的功能
+      },
+      {
+          "language": "html",
+          "autoFix": true
+      },
+  ], */
 }
